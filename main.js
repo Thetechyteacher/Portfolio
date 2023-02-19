1d70a2c27b95aec0faf4d0d8b0ddc3a176be7b95
Thetechyteacher <thetechyteacherr@gmail.com>
2023-02-19 17:02:39 +0000

    Added JS dark mode, linkedin and changed picture

---
+++ main.js
@@ -0,0 +1,53 @@
+
+
+
+let darkModeToggle = document.querySelector(".darkModeToggle");
+let aboutMeBackground= document.querySelector(".aboutMeContainer");
+
+darkModeToggle.addEventListener ("click", darkMode)
+
+
+
+window.addEventListener ("load", function getLocalstorage(){
+
+    if (localStorage.darkmode == "true"){
+        document.body.style.backgroundColor= "black"
+        aboutMeBackground.style. backgroundColor= "black"
+        document.body.style.color="white"
+        darkModeToggle.value="light"
+      
+    }
+    
+    else 
+    {
+        document.body.style.backgroundColor= "white"
+           document.body.style.color= "rgba(10, 2, 21, 0.578)"
+           aboutMeBackground.style. backgroundColor= "#F5F5F5"
+           darkModeToggle.value= "dark"
+    
+    }
+    
+
+})
+
+
+
+
+function darkMode () {
+    if (darkModeToggle.value == "dark"){
+         document.body.style.backgroundColor= "black"
+         aboutMeBackground.style. backgroundColor= "black"
+         document.body.style.color="white"
+         darkModeToggle.value="light"
+         localStorage.setItem("darkmode", true)
+     }
+     else{
+        document.body.style.backgroundColor= "white"
+        document.body.style.color= "rgba(10, 2, 21, 0.578)"
+        aboutMeBackground.style. backgroundColor= "#F5F5F5"
+        darkModeToggle.value= "dark"
+        localStorage.setItem("darkmode", false)
+     }
+     
+     
+ }
