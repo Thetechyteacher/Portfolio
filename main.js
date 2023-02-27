5bc6acd2355fdd12901106d84e900de5c90044db
Thetechyteacher <thetechyteacherr@gmail.com>
2023-02-27 17:07:49 +0000

    Fixed Dark Mode and updated picture

--- main.js
+++ main.js
@@ -1,60 +1,107 @@
 
 
 
-let darkModeToggle = document.querySelector(".darkModeToggle");
-let aboutMeBackground= document.querySelector(".aboutMeContainer");
-//let articleWrapper= document.querySelectorAll(".articlewrapper");
-//let articles= document.querySelector(".articles");
+// let darkModeToggle = document.querySelector(".darkModeToggle");
+// let aboutMeBackground= document.querySelector(".aboutMeContainer");
+// //let articleBody= document.querySelectorAll(".article-body");
+// let articles= document.querySelectorAll(".container");
 
-darkModeToggle.addEventListener ("click", darkMode)
+// darkModeToggle.addEventListener ("click", darkMode)
 
 
 
-window.addEventListener ("load", function getLocalstorage(){
+// window.addEventListener ("load", function getLocalstorage(){
 
-    if (localStorage.darkmode == "true"){
-        document.body.style.backgroundColor= "black"
-        aboutMeBackground.style. backgroundColor= "black"
-        document.body.style.color="white"
-        darkModeToggle.value="light"
+//     if (localStorage.darkmode == "true"){
+//         document.body.style.backgroundColor= "black"
+//         aboutMeBackground.style.backgroundColor= "black"
+//         //articleBody.style.backgroundColor= "black"
+//         articles.style.backgroundColor= "black"
+//         document.body.style.color="white"
+//         darkModeToggle.value="light"
       
-    }
+//     }
     
-    else 
-    {
-        document.body.style.backgroundColor= "white"
-           document.body.style.color= "rgba(10, 2, 21, 0.578)"
-           aboutMeBackground.style. backgroundColor= "#F5F5F5"
-           darkModeToggle.value= "dark"
+//     else 
+//     {
+//         document.body.style.backgroundColor= "white"
+//            document.body.style.color= "rgba(10, 2, 21, 0.578)"
+//            aboutMeBackground.style. backgroundColor= "#F5F5F5"
+//            darkModeToggle.value= "dark"
     
-    }
+//     }
     
 
-})
+// })
 
 
 
 
-function darkMode () {
+// function darkMode () {
    
-    if (darkModeToggle.value == "dark"){ 
+//     if (darkModeToggle.value == "dark"){ 
         
-         document.body.style.backgroundColor= "black";
-         aboutMeBackground.style.backgroundColor= "black";
-         //articles.style.backgroundColor="black";
-         //articleWrapper.style.backgroundColor="black";
+//          document.body.style.backgroundColor= "black";
+//          aboutMeBackground.style.backgroundColor= "black";
+//          articles.style.backgroundColor= "black";
+//          //articleBody.style.backgroundColor= "black";
          
-         document.body.style.color="white";
-         darkModeToggle.value="light";
-         localStorage.setItem("darkmode", true);
-     }
-     else{
-        document.body.style.backgroundColor= "white"
-        document.body.style.color= "rgba(10, 2, 21, 0.578)"
-        aboutMeBackground.style. backgroundColor= "#F5F5F5"
-        darkModeToggle.value= "dark"
-        localStorage.setItem("darkmode", false)
-     }
+//          document.body.style.color="white";
+//          darkModeToggle.value="light";
+//          localStorage.setItem("darkmode", true);
+//      }
+//      else{
+//         document.body.style.backgroundColor= "white"
+//         document.body.style.color= "rgba(10, 2, 21, 0.578)"
+//         aboutMeBackground.style. backgroundColor= "#F5F5F5"
+//         darkModeToggle.value= "dark"
+//         localStorage.setItem("darkmode", false)
+//      }
      
      
- }
+//  }
+let darkModeToggle = document.querySelector(".darkModeToggle");
+let aboutMeBackground = document.querySelector(".aboutMeContainer");
+let articles = document.querySelectorAll("article");
+let articleWrappers = document.querySelectorAll(".articlewrapper");
+
+darkModeToggle.addEventListener("click", darkMode);
+
+window.addEventListener("load", function getLocalStorage() {
+  if (localStorage.darkmode == "true") {
+    document.body.style.backgroundColor = "black";
+    aboutMeBackground.style.backgroundColor = "black";
+    articles.forEach((article) => (article.style.backgroundColor = "black"));
+    articleWrappers.forEach((wrapper) => (wrapper.style.backgroundColor = "black"));
+    document.body.style.color = "white";
+    darkModeToggle.value = "light";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "rgba(10, 2, 21, 0.578)";
+    aboutMeBackground.style.backgroundColor = "#F5F5F5";
+    articles.forEach((article) => (article.style.backgroundColor = "white"));
+    articleWrappers.forEach((wrapper) => (wrapper.style.backgroundColor = "white"));
+    darkModeToggle.value = "dark";
+  }
+});
+
+function darkMode() {
+  if (darkModeToggle.value == "dark") {
+    document.body.style.backgroundColor = "black";
+    aboutMeBackground.style.backgroundColor = "black";
+    articles.forEach((article) => (article.style.backgroundColor = "black"));
+    articleWrappers.forEach((wrapper) => (wrapper.style.backgroundColor = "black"));
+    document.body.style.color = "white";
+    darkModeToggle.value = "light";
+    localStorage.setItem("darkmode", true);
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "rgba(10, 2, 21, 0.578)";
+    aboutMeBackground.style.backgroundColor = "#F5F5F5";
+    articles.forEach((article) => (article.style.backgroundColor = "white"));
+    articleWrappers.forEach((wrapper) => (wrapper.style.backgroundColor = "white"));
+    darkModeToggle.value = "dark";
+    localStorage.setItem("darkmode", false);
+  }
+}
+
